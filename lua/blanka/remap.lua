c7eb46207f62df4db3ae1434f1fcc3f10b0b5c3d
HeloPras <maharjanprasiddha8@gmail.com>
2026-07-07 22:33:52 +0545

    added vim split

--- lua/blanka/remap.lua
+++ lua/blanka/remap.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set("i", "kj", "<Esc>", { noremap = true })
+
+
+
+-- error diagnostics
 vim.keymap.set("n","<leader>e",vim.diagnostic.open_float,{desc = "show line diagnostic"})
 vim.keymap.set("n","<leader>ne",function ()
 	vim.diagnostic.goto_next({float = true})
@@ -12,17 +16,36 @@ vim.keymap.set("n","<leader>pe",function ()
 	vim.diagnostic.goto_prev({float = true})
 end, {desc = 'Go to previous error and show float'})
 
+
+-- auto brackets and quotes
 vim.keymap.set('i',"(","()<Left>")
 vim.keymap.set('i',"{","{}<Left>")
 vim.keymap.set('i',"[","[]<Left>")
 vim.keymap.set('i',"'","''<Left>")
 vim.keymap.set('i','"','""<Left>')
+vim.keymap.set('i','<','<><Left>')
 
-vim.keymap.set("n","vie","ggVG")
+
+
+-- vim's window controller
+vim.keymap.set('n','<leader>nw','<cmd>vsplit<cr>',{desc = "split vertically"})
+vim.keymap.set('n','<leader>wn','<cmd>split<cr>',{desc = "split horizontally"})
+vim.keymap.set('n','<leader>wl','<C-w>l',{desc = "go to right split"})
+vim.keymap.set('n','<leader>wh','<C-w>h',{desc = "go to left split"})
+vim.keymap.set('n','<leader>wk','<C-w>k',{desc = "go to up split"})
+vim.keymap.set('n','<leader>wj','<C-w>j',{desc = "go to down split"})
+
+
+
+
+
+vim.keymap.set("n","vie","ggVG", {desc= "select all"})
+
 
 
 -- Normal mode: Toggle comment for the current line
 vim.keymap.set('n', '<C-/>', 'gcc', { remap = true, desc = 'Toggle comment line' })
+vim.keymap.set('i', '<C-/>', 'gcc', { remap = true, desc = 'Toggle comment line' })
 
 -- Visual mode: Toggle comment for the selected block
 vim.keymap.set('v', '<C-/>', 'gc', { remap = true, desc = 'Toggle comment selection' })
@@ -31,7 +54,6 @@ vim.keymap.set("n","gd",vim.lsp.buf.definition, {desc = "go definition"})
 
 vim.keymap.set("n", "<leader>tm", "<cmd>terminal<cr>", { noremap = true })
 
-vim.keymap.set("n", "<leader>ls", '<cmd>!firefox "%"<cr>', { desc = "Open file in firefox" })
 
 
 
