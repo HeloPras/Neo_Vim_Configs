862df036d959603aec8cf921d80c956845ba8299
HeloPras <maharjanprasiddha8@gmail.com>
2026-07-15 23:44:53 +0545

    added go definition remap in telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,12 +1,26 @@
-
-
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
 
+
+require('telescope').setup({
+	defaults = {
+		mappings = {
+			n = {
+				["<C-l>"] = actions.select_vertical,
+				["<C-v>"] = false,
+
+			}
+		}
+	}
+})
 
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope git file' })
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search  = vim.fn.input("Grep > ") });
+	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', 'gd', builtin.lsp_definitions, { desc = 'opens diagnostic in telescope' })
+
+vim.keymap.set('n', '<leader>qf', builtin.quickfix, { desc = "opens quick fix" })
--- lua/blanka/remap.lua
+++ lua/blanka/remap.lua
@@ -7,42 +7,42 @@ vim.keymap.set("i", "kj", "<Esc>", { noremap = true })
 
 
 -- error diagnostics
-vim.keymap.set("n","<leader>e",vim.diagnostic.open_float,{desc = "show line diagnostic"})
-vim.keymap.set("n","<leader>ne",function ()
-	vim.diagnostic.goto_next({float = true})
-end, {desc = 'Go to next error and show float'})
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "show line diagnostic" })
+vim.keymap.set("n", "<leader>ne", function()
+	vim.diagnostic.goto_next({ float = true })
+end, { desc = 'Go to next error and show float' })
 
 
 
-vim.keymap.set("n","<leader>pe",function ()
-	vim.diagnostic.goto_prev({float = true})
-end, {desc = 'Go to previous error and show float'})
+vim.keymap.set("n", "<leader>pe", function()
+	vim.diagnostic.goto_prev({ float = true })
+end, { desc = 'Go to previous error and show float' })
 
 
 -- auto brackets and quotes
-vim.keymap.set('i',"(","()<Left>")
-vim.keymap.set('i',"{","{}<Left>")
-vim.keymap.set('i',"[","[]<Left>")
-vim.keymap.set('i',"'","''<Left>")
-vim.keymap.set('i','"','""<Left>')
+vim.keymap.set('i', "(", "()<Left>")
+vim.keymap.set('i', "{", "{}<Left>")
+vim.keymap.set('i', "[", "[]<Left>")
+vim.keymap.set('i', "'", "''<Left>")
+vim.keymap.set('i', '"', '""<Left>')
 -- vim.keymap.set('i','<','<><Left>')
-vim.keymap.set('i','`','``<Left>')
+vim.keymap.set('i', '`', '``<Left>')
 
 
 
 -- vim's window controller
-vim.keymap.set('n','<leader>nw','<cmd>vsplit<cr>',{desc = "split vertically"})
-vim.keymap.set('n','<leader>wn','<cmd>split<cr>',{desc = "split horizontally"})
-vim.keymap.set('n','<leader>wl','<C-w>l',{desc = "go to right split"})
-vim.keymap.set('n','<leader>wh','<C-w>h',{desc = "go to left split"})
-vim.keymap.set('n','<leader>wk','<C-w>k',{desc = "go to up split"})
-vim.keymap.set('n','<leader>wj','<C-w>j',{desc = "go to down split"})
+vim.keymap.set('n', '<leader>nw', '<cmd>vsplit<cr>', { desc = "split vertically" })
+vim.keymap.set('n', '<leader>wn', '<cmd>split<cr>', { desc = "split horizontally" })
+vim.keymap.set('n', '<leader>wl', '<C-w>l', { desc = "go to right split" })
+vim.keymap.set('n', '<leader>wh', '<C-w>h', { desc = "go to left split" })
+vim.keymap.set('n', '<leader>wk', '<C-w>k', { desc = "go to up split" })
+vim.keymap.set('n', '<leader>wj', '<C-w>j', { desc = "go to down split" })
 
 
 
 
 
-vim.keymap.set("n","vie","ggVG", {desc= "select all"})
+vim.keymap.set("n", "vie", "ggVG", { desc = "select all" })
 
 
 
@@ -53,22 +53,5 @@ vim.keymap.set('i', '<C-/>', 'gcc', { remap = true, desc = 'Toggle comment line'
 -- Visual mode: Toggle comment for the selected block
 vim.keymap.set('v', '<C-/>', 'gc', { remap = true, desc = 'Toggle comment selection' })
 
-vim.keymap.set("n","gd",vim.lsp.buf.definition, {desc = "go definition"})
 
 vim.keymap.set("n", "<leader>tm", "<cmd>terminal<cr>", { noremap = true })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
